2e4f4280062103d9239f93e5d51553fc59f6c177
ae7ffl <lucia@kai-zen.es>
2026-08-28 16:25:27 +0200

    bin_confi

--- js/jsonbin-config.js
+++ js/jsonbin-config.js
@@ -2,6 +2,6 @@
 // Están explicados paso a paso en el README.md ("1. Configurar jsonbin.io").
 
 const JSONBIN_CONFIG = {
-  binId: "TU_BIN_ID_AQUI",       // lo ves en la URL del bin en tu dashboard
-  masterKey: "TU_MASTER_KEY_AQUI" // Account → API Keys → tu X-Master-Key
+  binId: "6a919a30f5f4af5e294e1841",       // lo ves en la URL del bin en tu dashboard
+  masterKey: "$2a$10$ibKsA4IJC5cTOVxXIPgffuvRwkfFFfwJn8hh9Agj60ic/xQs86zE." // Account → API Keys → tu X-Master-Key
 };
